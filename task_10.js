bc110127270a10d37d3c276f193a74e9ad5abed5
Rumata-SL <kirr7619@gmail.com>
2022-03-04 01:00:24 +0500

    task kyu 8 "Grasshopper - Basic Function Fixer"

---
+++ task_10.js
@@ -0,0 +1,17 @@
+"use strict";
+// task kyu 8 "Grasshopper - Basic Function Fixer"
+/*
+    I created this function to add five to any number
+     that was passed in to it and return the new value.
+      It doesn't throw any errors but it returns the wrong number.
+ */
+
+/*
+    Я создал эту функцию, чтобы добавить пять к любому
+    переданному ей числу и вернуть новое значение.
+     Он не выдает никаких ошибок, но возвращает неправильный номер.
+ */
+function addFive(num) {
+    let total = num + 5;
+    return total;
+}
