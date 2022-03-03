5c5861340d4f9c9ed71d51e3aeb8cf20cd38aad4
Rumata-SL <kirr7619@gmail.com>
2022-03-04 01:24:27 +0500

    task kyu 8 "Reverse List Order"

---
+++ task_12.js
@@ -0,0 +1,14 @@
+"use strict";
+// task kyu 8 "Reverse List Order"
+/*
+    In this kata you will create a function
+     that takes in a list and returns a list with the reverse order.
+*/
+
+/*
+    В этом ката вы создадите функцию, которая принимает список
+     и возвращает список в обратном порядке.
+*/
+function reverseList(list) {
+    return list.reverse();
+}
