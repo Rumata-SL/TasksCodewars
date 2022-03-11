e6702b587248c2b22a6f536f3dae8605cab85fc0
Rumata-SL <kirr7619@gmail.com>
2022-03-12 01:53:56 +0500

    task kyu 7 "Sum of odd numbers"

---
+++ task_20.js
@@ -0,0 +1,8 @@
+"use strict";
+// task kyu 7 "Sum of odd numbers"
+/*Given the triangle of consecutive odd numbers:*/
+
+/*Дан треугольник последовательных нечетных чисел:*/
+function rowSumOddNumbers(n) {
+    return n * n * n;
+}
