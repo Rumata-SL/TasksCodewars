bbc526be8f4a21619da10842ed2b7d1a4ce2231b
Rumata-SL <kirr7619@gmail.com>
2022-03-12 22:17:58 +0500

    task kyu 8 "Get the mean of an array"

---
+++ tasks_21-30/task_22.js
@@ -0,0 +1,26 @@
+"use strict";
+// task kyu 8 "Get the mean of an array"
+/*
+    It's the academic year's end, fateful moment of your school report.
+     The averages must be calculated. All the students come
+     to you and entreat you to calculate their average for them.
+      Easy ! You just need to write a script.
+    Return the average of the given array rounded down to its nearest integer.
+    The array will never be empty.
+*/
+
+/*
+    Конец учебного года, судьбоносный момент твоего школьного отчета.
+     Средние значения должны быть рассчитаны. Все ученики подходят к вам
+      и умоляют вас подсчитать для них их средний балл.
+       Легкий ! Вам просто нужно написать сценарий.
+    Возвращает среднее значение данного массива, округленное до ближайшего целого числа.
+    Массив никогда не будет пустым.
+*/
+function getAverage(marks) {
+    let res = 0;
+    for (let i = 0; i < marks.length; i++) {
+        res += marks[i];
+    }
+    return ~~(res / marks.length);
+}
