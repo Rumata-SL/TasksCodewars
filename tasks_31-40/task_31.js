a5e7f11a2b87807df20c3995c2e408d24afe3925
Rumata-SL <kirr7619@gmail.com>
2022-03-19 22:11:36 +0500

    task kyu 5 "Moving Zeros To The End"

--- tasks_31-40/task_31.js
+++ tasks_31-40/task_31.js
@@ -1,2 +1,23 @@
 "use strict";
-// task kyu 6 ""
+// task kyu 5 "Moving Zeros To The End"
+/*
+    Write an algorithm that takes an array and moves
+    all of the zeros to the end, preserving the order of the other elements.
+*/
+
+/*
+    Напишите алгоритм, который берет массив и перемещает
+     все нули в конец, сохраняя порядок остальных элементов.
+ */
+function moveZeros(arr) {
+    const res = []
+    const res1 = []
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 0) {
+            res.push(arr[i])
+        } else {
+            res1.push(arr[i])
+        }
+    }
+    return [...res1, ...res]
+}
