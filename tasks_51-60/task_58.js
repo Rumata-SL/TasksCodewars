e25eb9d1b286afb1f8792e093b3fd97f34a279fd
Rumata-SL <kirr7619@gmail.com>
2022-05-22 00:21:14 +0500

    task kyu 8 "Count Odd Numbers below n"

---
+++ tasks_51-60/task_58.js
@@ -0,0 +1,9 @@
+"use strict";
+// task kyu 8 "Count Odd Numbers below n"
+/*Given a number n, return the number of positive odd numbers below n, EASY!*/
+
+/*Учитывая число n, вернуть количество положительных нечетных чисел ниже n, ЛЕГКО!*/
+
+function oddCount(n) {
+    return Math.floor(n/2)
+}
