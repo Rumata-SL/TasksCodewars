4383a24cea1a8aacbe2ac9cd8ffe8cec4cc79d7c
Rumata-SL <kirr7619@gmail.com>
2022-07-15 22:22:42 +0500

    task kyu 8 "Convert a Boolean to a String"

---
+++ tasks_81-90/task_82.js
@@ -0,0 +1,11 @@
+"use strict";
+// task kyu 8 "Convert a Boolean to a String"
+/*Implement a function which convert the given boolean value into its string representation.
+Note: Only valid inputs will be given.*/
+
+/*Реализуйте функцию, которая преобразует данное логическое значение в его строковое представление.
+Примечание. Будут даны только действительные входные данные.
+*/
+function booleanToString(b) {
+    return b + ""
+}
