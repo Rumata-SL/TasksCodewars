c3916d66766f2abf13a3c79c54966048a4457ec9
Rumata-SL <kirr7619@gmail.com>
2022-07-15 22:19:12 +0500

    task kyu 8 "Function 3 - multiplying two numbers"

---
+++ tasks_81-90/task_81.js
@@ -0,0 +1,7 @@
+"use strict";
+// task kyu 8 "Function 3 - multiplying two numbers"
+/*Implement a function which multiplies two numbers.*/
+
+/*Реализуйте функцию, которая умножает два числа.*/
+
+const mult = (a, b )=>a*b
