a242596eb2cc906995a58e1c8dfef3bfdab70b58
Rumata-SL <kirr7619@gmail.com>
2022-07-31 21:15:33 +0500

    task kyu 8 "Basic variable assignment"

---
+++ tasks_81-90/task_86.js
@@ -0,0 +1,7 @@
+"use strict";
+// task kyu 8 "Basic variable assignment"
+/*This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?*/
+/*Этот код должен храниться "codewa.rs"как вызываемая переменная, nameно он не работает. Вы можете понять, почему?*/
+let a = "code";
+let b = "wa.rs";
+let name = a + b;
