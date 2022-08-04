3396efc535e8d6cef68398b8450d70c614021eea
Rumata-SL <kirr7619@gmail.com>
2022-08-04 22:08:58 +0500

    task kyu 8 "Grasshopper - Check for factor"

---
+++ tasks_81-90/task_87.js
@@ -0,0 +1,11 @@
+"use strict";
+// task kyu 8 "Grasshopper - Check for factor"
+/*This function should test if the factor is a factor of base.
+Return true if it is a factor or false if it is not.*/
+
+/*Эта функция должна проверять, factorявляется ли фактор фактором base.
+Возврат true, если это фактор или falseесли это не так.*/
+
+function checkForFactor(base, factor) {
+    return base % factor === 0
+}
