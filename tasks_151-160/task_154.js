76c99ac6c4b3a0b682173087e04c7a86c4288b3c
Rumata-SL <kirr7619@gmail.com>
2022-09-15 22:02:30 +0500

    task kyu 7 "Sort arrays - 1"

---
+++ tasks_151-160/task_154.js
@@ -0,0 +1,7 @@
+// task kyu 7 "Sort arrays - 1"
+/*Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.*/
+/*Простое использование сортировки. Создайте функцию, которая возвращает элементы входного массива/списка, отсортированные в лексикографическом порядке.*/
+
+const sortme = function( names ){
+return names.sort()
+}
